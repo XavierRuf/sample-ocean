9e9726647262da20977ba419f1322507dda9b475
XavierRuf <romanyukrafail94@gmail.com>
2021-06-11 15:43:31 +0300

    second-commit

--- components/PriceCard/PriceCard.js
+++ components/PriceCard/PriceCard.js
@@ -13,19 +13,18 @@ function PriceCard({title, footer, type}) {
        },   7000);
     }, [])
 
-    let addColor = state.percent > 0;
+    const bitcoin = type === 'bitcoin';
     return (
         <div>
-            <div className={styles.price}>
+            <div className={`${styles.price} ` + `${bitcoin ? styles.price__bitcoin : styles.price__etherium}`}>
                 <div className={styles.price__container}>
                     <p className={styles.price__title}>{title}</p>
                     <div className={styles.price__wrapp}>
                         <p className={styles.price__coin}>${state.price}</p> 
-                        <p className={`${addColor ? styles.plus : styles.minus} ${styles.price__percent}`}>{state.percent}<span>%</span></p>
-
+                        <p className={`${state.percent > 0 ? styles.plus : styles.minus} ${styles.price__percent}`}>{state.percent}<span>%</span></p>
                         {state.percent > 0 ? <span>&uarr;</span> : <span>&darr;</span>}
                     </div>
-                <p className={styles.price__footer}>{footer}</p>
+                    <p className={styles.price__footer}>{footer}</p>
                 </div>
             </div>
         </div>
