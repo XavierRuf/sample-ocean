02c6360a868a76778b9c610e1696bb69e75b715c
XavierRuf <romanyukrafail94@gmail.com>
2021-06-11 17:23:16 +0300

    first-updated

--- components/PriceWrapper/PriceWrapper.js
+++ components/PriceWrapper/PriceWrapper.js
@@ -3,7 +3,7 @@ import PriceCard from '../PriceCard/PriceCard';
 import styles from './PriceWrapper.module.scss';
 function PriceWrapper() {
     return (
-        <div className={styles.pirce__wrapper}>
+        <div className={styles.price__wrapper}>
             {constants.cardsData.map((item) => 
                 <PriceCard title = {item.title} footer = {item.footer} type = {item.key} key = {item.key} className={item.name}/>
             )}
